5ede7001dcff46bc368d4ebbe3fc87a8229f9a8a
Marcelo Romero <romemarce1@gmail.com>
2023-04-10 21:14:46 -0300

    update structure

---
+++ src/js/panel.js
@@ -0,0 +1,63 @@
+import { initDatabase, getDatabase, setDatabase } from './database.js'
+import { showHeaderPanel } from './routes.js'
+showHeaderPanel()
+initDatabase()
+
+
+
+const createCarrera = (nombre, tipo, duracion, modalidad) => {
+  // Obtenemos la database
+  const database = getDatabase()
+  // Creamos nueva carrea
+  const nuevo = {
+    id: database.carreras.length,
+    nombre,
+    tipo,
+    duracion,
+    modalidad
+  }
+  // Agregamos elemento a carrear
+  database.carreras.push( nuevo )
+  // Guardamos la carrera
+  setDatabase( database )
+}
+
+const getCarreras = () => {
+  // Obtenemos la database
+  const database = getDatabase()
+  return database.carreras
+}
+console.log("Carreras",getCarreras());
+
+// update
+const updateCarrera = (id,nombre, tipo, duracion, modalidad) => {
+  // Obtenemos la database
+  const database = getDatabase()
+  if (database.carreras) {
+    database.carreras.forEach(carrera => {
+      if (carrera.id === id) {
+        carrera.nombre = nombre
+        carrera.tipo = tipo
+        carrera.duracion = duracion
+        carrera.modalidad = modalidad
+      }
+    });
+    setDatabase( database )
+  }
+}
+// updateCarrera(2,"Contador","Grado",5,"Presencial")
+// delete
+
+const deleteCarrera = (id) => {
+  // Obtenemos la database
+  const database = getDatabase()
+  if (database.carreras) {
+    const aux = []
+    database.carreras.forEach(carrera => {
+      if (carrera.id !== id) {
+        aux.push(carrera)
+      }
+    });
+    setDatabase({ ...database, carreras: aux })
+  }
+} // deleteCarrera(1)
--- src/js/routes.js
+++ src/js/routes.js
@@ -16,12 +16,36 @@ const routes = [
   {
     name: "Carreras",
     url: "./carreras.html"
-  },
+  }
 ]
 
 export const showHeaderMenu = ()=>{
   menuDom.innerHTML = "<ul>"
   routes.forEach(route => {
+    menuDom.innerHTML += `<li><a href="${route.url}">${route.name}</a></li>`
+  })
+  menuDom.innerHTML += `<li><button>Bedelia</button></li>`
+  menuDom.innerHTML += "</ul>"
+}
+
+
+const routesPanel = [
+  {
+    name: "Estudiantes",
+    url: "./index.html"
+  },
+  {
+    name: "Carreras",
+    url: "./index.html"
+  },
+  {
+    name: "Materias",
+    url: "./index.html"
+  },
+]
+export const showHeaderPanel = ()=>{
+  menuDom.innerHTML = "<ul>"
+  routesPanel.forEach(route => {
     menuDom.innerHTML += `<li><a href="${route.url}">${route.name}</a></li>`
   })
   menuDom.innerHTML += "</ul>"
