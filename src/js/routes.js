3a2175803754d1d0767166e3e583c7ae581d6da5
Marcelo <romemarce1@gmail.com>
2023-04-13 21:37:37 -0300

    Structure panel files

--- src/js/routes.js
+++ src/js/routes.js
@@ -17,52 +17,20 @@ const routes = [
     name: "Carreras",
     url: "./carreras.html",
   },
+  {
+    name: "Bedelia",
+    url: "./panel.html",
+    className: "bedelia"
+  },
 ];
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  window.location = "bedelia.html";
-};
-const createModalLogin = (elementDom) => {
-  const sectionDom = document.createElement("section");
-  sectionDom.classList.add("content-modal", "active");
-
-  const formDom = document.createElement("form");
-  formDom.classList.add("modal");
-  formDom.onsubmit = (e) => handleSubmit(e);
-  // Solo vistoso
-  formDom.innerHTML = `
-        <input type="text" name="user" placeholder="Usuario" />
-        <input type="text" name="pass" placeholder="Contraseña" />
-        <button type="submit">Enviar</button>
-    `;
-  const buttonDom = document.createElement("button");
-  buttonDom.classList.add('close-modal')
-  buttonDom.onclick = (e) => sectionDom.remove();
-  buttonDom.innerText = "x";
-
-  formDom.prepend(buttonDom);
-  sectionDom.append(formDom);
-
-  elementDom.append(sectionDom);
-};
 
 export const showHeaderMenu = () => {
   const ulDom = document.createElement("ul");
+  ulDom.classList.add('menu-list')
   routes.forEach((route) => {
-    ulDom.innerHTML += `<li><a href="${route.url}">${route.name}</a></li>`;
+    ulDom.innerHTML += `<li class="menu-item ${route?.className ? route.className : "" }"><a href="${route.url}">${route.name}</a></li>`;
   });
-  const liDom = document.createElement("li");
-
-  const buttonDom = document.createElement("button");
-  buttonDom.onclick = (e) => createModalLogin(menuDom);
-  buttonDom.innerText = "Bedelia";
-  liDom.append(buttonDom);
-  ulDom.append(liDom);
-
   menuDom.append(ulDom);
-
-  // Add Logint to menu
 };
 
 // form
@@ -70,15 +38,15 @@ export const showHeaderMenu = () => {
 const routesPanel = [
   {
     name: "Estudiantes",
-    url: "./index.html",
+    url: "./panel-estudiante.html",
   },
   {
     name: "Carreras",
-    url: "./index.html",
+    url: "./panel-carrera.html",
   },
   {
     name: "Materias",
-    url: "./index.html",
+    url: "./panel-materia.html",
   },
 ];
 export const showHeaderPanel = () => {
