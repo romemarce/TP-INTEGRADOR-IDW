5dbefe2485097e8a562e528628d25ac3de51e02d
Facundo <facundopanozzo@gmail.com>
2023-04-17 22:29:07 -0300

    json de contactos

--- src/js/contacto.js
+++ src/js/contacto.js
@@ -1,11 +1,17 @@
 const URL = '/src/db/contactos.json'
-fetch(URL)
-.then(response => response.json())
-.then(datos=>{
-    let listDom = "";
-    for ({name,tel} of datos) {
-        listDom += `<tr><td>${name}</td>
-                    <td>${tel}</td></tr>`;
-    }
-    document.getElementById("contactos").innerHTML = listDom;
-});
+async function Contact(){
+   const res= await fetch(URL)
+   const data= await res.json();
+            let listDom = "";
+            for ({name,tel} of data) {
+                listDom += `<tr><td>${name}</td>
+                            <td>${tel}</td></tr>`;
+            }
+            document.getElementById("contactos").innerHTML = listDom;
+}
+
+async function Buscar(){
+
+}
+
+Contact();
